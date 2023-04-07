fa5b0d7083fa96066857e0acd0b6eba12bde44c2
Insaner <dbfpzk2002@gmail.com>
2023-04-07 21:45:37 +0900

    update

--- src/chap_03/_04_NestedLoop.java
+++ src/chap_03/_04_NestedLoop.java
@@ -11,5 +11,59 @@ public class _04_NestedLoop {
             }
             System.out.println();
         }
+
+        for (int i = 1; i  < 10 ; i++) {
+            for (int j = 1; j < 10 ; j++) {
+                System.out.println(i+" X "+ j + " = " + (i*j) );
+
+            }
+
+        }
+        //break 문
+        int a = 10;
+        for (int i = 1; i < 20; i++) {
+            System.out.println(i +"번째");
+            if (i == a){
+                System.out.println("끝!");
+                break;
+            }
+        }
+
+        int max = 10; //최대판매수량
+        int sold = 0; //현재판매수량
+        int noShow = 7;
+        for (int i = 1; i < 20 ; i++) {
+            System.out.println(i+"번째");
+
+            if(i == noShow){
+                System.out.println(i+"번 없음");
+                continue; //이 구문 이후는 실행되지 않고 반복문 처음으로 돌아감
+            }
+
+            sold++; //판매처리
+            if(sold==max){
+                System.out.println("끝");
+                break;
+            }
+        }
+        System.out.println("반복문 종료");
+
+        int price = 0;
+        int hour = 5;
+        boolean isSmallcar = false;
+        boolean withDisabledCar = false;
+        for (int i = 0; i < hour; i++) {
+            price = 4000;
+            if(price>30000){
+                price = 30000;
+                System.out.println("최대 금액입니다");
+            }
+        }
+        if(!isSmallcar){
+            System.out.println(price);
+        }else {
+            System.out.println(price/2);
+        }
+
     }
 }
---
+++ src/chap_04/_01_array.java
@@ -0,0 +1,21 @@
+package chap_04;
+
+public class _01_array {
+    public static void main(String[] args) {
+
+        //String[] orders = new String[4];
+        //String orders[] new String[4];
+        //String[] orders = new String[]{"유유","유유","유유","유유"};
+        String[] orders = {"유유","유유","유유","유유"};
+
+        for(int i=0; i<orders.length;i++){
+            System.out.println(orders[i]);
+        }
+        //위랑 같은 코드 좀 더 간결하게 사용가능  for each
+        for (String order : orders ){
+            System.out.println(order);
+        }
+
+
+    }
+}
